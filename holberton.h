773501e29e35dd3a1256a0072d0a437cf1689e39
sranciato <sranciato1@gmail.com>
2019-07-29 16:12:43 +0000

    fix comments for struct

--- holberton.h
+++ holberton.h
@@ -9,8 +9,15 @@
 #define LENGTH(a) (sizeof(a) / sizeof((a)[0]))
 
 typedef void (*Printer)(va_list);
-
-typedef struct {
+/**
+ * struct Spec - function pointer and specifier.
+ * @name: name of specifier.
+ * @f: function pointer for get spec.
+ *
+ * Description: Specifier types.
+ */
+typedef struct Spec
+{
 	char name;
 	Printer f;
 } Spec;
