ea1b3abb21af1526c9f26a42790c7b428ac4767d
Nathaniel <821@holbertonschool.com>
2019-07-29 13:02:56 -0400

    fix printing large/negative/0 binary numbers

--- print_b.c
+++ print_b.c
@@ -6,12 +6,18 @@
  */
 void print_b(va_list args)
 {
-	int a[10], i, n = va_arg(args, int);
+	int a[32], i;
+	unsigned int n = va_arg(args, unsigned int);
 
-	for (i = 0; n > 0; i++)
+	if (n == 0)
 	{
-		a[i] = n % 2;
-		n /= 2;
+		outc('0');
+		return;
+	}
+	for (i = 0; n != 0; i++)
+	{
+		a[i] = n & 1;
+		n >>= 1;
 	}
 	for (i = (i - 1); i >= 0; i--)
 	{
